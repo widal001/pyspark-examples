bea40204502e22887b4782ee428acbdfeecc6896
widal001 <williamdaly422@gmail.com>
2022-05-18 14:29:27 -0400

    ci: Fixes failing CI/CD test because of parallelization
    The previous test_word_counts() expected a certain value to be returned first from the RDD. This change checks that all of the correct frequencies are returned in any order

--- tests/unit_tests/test_example.py
+++ tests/unit_tests/test_example.py
@@ -21,9 +21,10 @@ def test_word_count(test_spark):
     """Tests the word_count() function"""
     # setup
     input_list = ["a", "b", "c", "a", "a", "b", "c", "d"]
+    expected = {("a", 3), ("c", 2), ("d", 1), ("b", 2)}
     rdd = example.rdd_from_list(test_spark, input_list)
     # execution
     counts = example.word_count(rdd)
     # validation
     assert counts.count() == 4
-    assert counts.take(1) == [("a", 3)]
+    assert set(counts.collect()) == expected
