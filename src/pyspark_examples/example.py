0d10b904b0eb64dfd14b019eadf43915684f516f
Billy Daly <williamdaly422@gmail.com>
2022-05-18 14:35:06 -0400

    feat: Implements `word_count()` function (#5)
    `word_count()` returns another RDD that maps each distinct word in a word list to its frequency in that list

--- src/pyspark_examples/example.py
+++ src/pyspark_examples/example.py
@@ -12,3 +12,13 @@ def rdd_from_list(spark: SparkSession, input_list: list) -> RDD:
     """Create an RDD from a list"""
     sc: SparkContext = spark.sparkContext
     return sc.parallelize(input_list)
+
+
+def word_count(rdd: RDD) -> RDD:
+    """Finds the frequency of each item in the RDD"""
+    counts = (
+        rdd.map(lambda word: word.lower())  # make lowercase to avoid dupes
+        .map(lambda word: (word, 1))  # map words to 1 to sum frequency
+        .reduceByKey(lambda a, b: a + b)  # sum the frequencies of each word
+    )
+    return counts
